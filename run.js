94daaf0e96abec03612077c5b8da2578f4690fd0
Sydney <agul85292@gmail.com>
2020-06-22 21:05:52 +0300

    new

--- run.js
+++ run.js
@@ -131,7 +131,7 @@ channels.forEach(function(entry) {
     }
     function bot2() {
         client.say(entry,"Aşağıdaki linklerden bize bağışta bulunabilirsiniz :) " +
-            "https://www.bynogame.com/destekle/theokoles - https://www.oyunfor.com/twitch-donate/theokoles - https://streamelements.com/theokoles/tip + https://www.perdigital.com/yayinciya-destek/theokoles" +
+            "https://www.bynogame.com/destekle/theokoles - https://www.oyunfor.com/twitch-donate/theokoles" +
 
             " ");
     }
