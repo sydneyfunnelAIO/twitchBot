3968d04b964821f0a4f99af85d011aa848080ac3
sydneyfunnelAIO <sydneyfunnelAIO@users.noreply.github.com>
2020-09-03 21:02:13 +0300

    new

--- run.js
+++ run.js
@@ -52,6 +52,9 @@ catch(err) {
   }
   var client = new tmi.client(config)
   client.connect()
+  client.on("subscription", (channel, username, method, message, userstate) => {
+    client.say(channel, '@'+username+'abone oldugun icin tesekkurler <3' )
+});
 
   client.on("chat", (channel, userstate, message, self) =>{
     if(self) return;
